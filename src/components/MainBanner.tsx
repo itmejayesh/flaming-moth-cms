bbe3b6d8913ee02a08f3c3ed325bcc391d764ea3
Jayesh Gadhok <jayeshgadhok@gmail.com>
2024-05-28 20:00:46 +0530

    70% work comeplet on home page

---
+++ src/components/MainBanner.tsx
@@ -0,0 +1,40 @@
+"use client";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import {bannerImages} from "@/constants/constants";
+import Autoplay from "embla-carousel-autoplay";
+import {useRef} from "react";
+
+function MainBanner() {
+	const autoplayInstance = useRef(Autoplay());
+	return (
+		<Carousel
+			className=""
+			opts={{loop: true}}
+			plugins={[autoplayInstance.current]}
+		>
+			<CarouselContent>
+				{bannerImages.map((img, index) => (
+					<CarouselItem key={index}>
+						<div>
+							<Image
+								src={img}
+								alt=""
+								width={1920} // Adjust width as per your high-resolution image
+								height={1080} // Adjust height as per your high-resolution image
+								quality={100} // Increase quality to 100
+								className="object-cover h-screen w-screen"
+							/>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+		</Carousel>
+	);
+}
+
+export default MainBanner;
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const bannerImages = [
+	"/assets/01.jpg",
+	"/assets/02.jpg",
+	"/assets/03.jpg",
+];
